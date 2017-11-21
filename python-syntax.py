325656eea37bf00fe2fb45a585530f197a71a572
mattperkins <github@mattperkins.com>
2017-11-21 14:41:20 +0000

    cl

--- python-syntax.py
+++ python-syntax.py
@@ -20,11 +20,20 @@
 
 
 
-    # functions
-def lemon(a = "default",b = "default"):
-    print('{},{}'.format(a,b))
-lemon("Hello")
-lemon(a="Unique", b="Value Added")
+#     # functions
+# def lemon(a = "default",b = "default"):
+#     print('{},{}'.format(a,b))
+# lemon("Hello")
+# lemon(a="Unique", b="Value Added")
+
+def radius(radius):
+    return 3.142 * radius * radius
+def volume(radius, length):
+    print("The total is:", int(radius(15)*length))
+
+
+length = 20
+volume(radius,length)
 
 
 
