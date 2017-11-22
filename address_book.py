72492aa44a95b1a96e8b64ffc66e52d0e3cdfdc0
mattperkins <github@mattperkins.com>
2017-11-22 00:06:37 +0000

    add address book app

---
+++ address_book.py
@@ -0,0 +1,18 @@
+def contacts(dict):
+    for key,val in dict.items():
+        print("My name is {} and I live in {}".format(key, val))
+
+hometowns = {}
+
+while True:
+    name = input('Enter a name:')
+    city = input('Enter a city:')
+    hometowns[name] = city
+
+    another = input('add another? (y/n)')
+    if another == 'y':
+        continue
+    else:
+        break
+
+contacts(hometowns)
