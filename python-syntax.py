8192be5ab0316dfde0ecb016095707bf711cb94e
mattperkins <github@mattperkins.com>
2017-11-22 00:58:58 +0000

    add set and sorted

--- python-syntax.py
+++ python-syntax.py
@@ -46,6 +46,7 @@
 
 
 
+
 #     # dictionaries
 # hometowns = {
 #     "fred" : "LA",
@@ -56,6 +57,10 @@
 
 # print("fred" in hometowns)
 # print("bar" in hometowns)
+
+# lemon = {1:2, 2:1}
+# print(lemon.keys())
+# print(lemon.values())
 
 # print(hometowns.keys())
 #     # can be typecast to a list 
@@ -133,6 +138,26 @@
 #     print(b)
 #     a, b = b, a + b
 # print("Done!")
+
+
+
+
+
+    # sorting and sets
+nums = [1,55,55,99,1,9,7,6,3]
+print(sorted(nums))
+    # capitals will be sorted first 
+names = ["bob", "amy", "Fred", "Lottie"]
+print(sorted(names))
+
+    # removes duplicates but doesn't preserve order
+print(set(nums))
+
+    # use with multiple methods
+lemon = {1:2, 1:3, 2:1, 2:2}
+print(lemon.keys())
+print(lemon.values())
+print(set(lemon.values()))
 
 
 
