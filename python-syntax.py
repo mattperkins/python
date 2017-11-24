acc35ad9be61016a930f65e783376d771330bfad
mattperkins <github@mattperkins.com>
2017-11-24 18:32:04 +0000

    add class and static methods

--- python-syntax.py
+++ python-syntax.py
@@ -149,6 +149,7 @@ class Lemon:
      self.age = 34
      self.city = "LA"
     
+    # instance method
     def business(self):
         return f"{self.name} works in {self.city}"
 
@@ -156,8 +157,41 @@ lime = Lemon()
 print(f'{lime.name} is {lime.age}')
 print(lime.business())
 
+    # create new 'class' instance (Person)
+class Person:
+    # class level attribute
+    species = 'human'
 
+    # instance attributes
+    def __init__(self, name, age, city):
+        self.name = name
+        self.age = age  
+        self.city = city
 
+    # class method
+    @classmethod
+    def classy(cls):
+        return f'on earth there are {cls.species}'
+    
+    # static method
+    @staticmethod
+    def walk(pace = 'approx. 1.3mph'):
+        return f'walks at {pace}'
+
+    # instance of Person
+Sandy = Person('Sandy', 24, 'LA')
+print(f'{Sandy.name}')
+
+Bob = Person('Bob', 55, 'NYC')
+print(f'{Bob.name} is a {Bob.species} and lives in {Bob.city}') 
+
+Lottie = Person('Lottie', '27', 'London')
+print(f'{Lottie.name} knows {Lottie.classy()}')
+    
+print(Person.classy())
+
+print(f'{Lottie.name} {Lottie.walk()}')
+print(f'{Bob.name} {Bob.walk("approx 1.5mph")}')
 
 
 #     # sorting and sets
